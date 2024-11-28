803d3f7f238fca3a05aca0bb45d51bf2db4b73b6
krolik131 <kostya-leteha@mail.ru>
2024-11-28 16:08:06 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,12 +1,8 @@
 def add_everything_up(a,b):
     try:
-        add = a+b
-        return add
-
-    except TypeError as e:
-        print(e)
-
-        return f'{a,b}'
+        return a + b
+    except TypeError as exc:
+        return str(a) + str(b)#выводит ошибку
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
